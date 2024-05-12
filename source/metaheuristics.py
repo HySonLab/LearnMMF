f9dba3219a68402adf3d6eab3a2fa1942c33a9f0
nguyenthieukhang <49133713+nguyenthieukhang@users.noreply.github.com>
2024-05-12 12:59:14 +0700

    Add DE

--- source/metaheuristics.py
+++ source/metaheuristics.py
@@ -1,13 +1,39 @@
 import torch
+import math
 import random
 import numpy as np
-from learn_single_mmf import train_single_mmf
+from learnable_mmf_model import learnable_mmf_train
+
+
+def generate_single_random_weighted_graph_laplacian(matrix_size, edge_probability, device='cpu'):
+    # Generate adjacency matrix
+    adjacency_matrix = torch.rand(matrix_size, matrix_size, device=device) < torch.sqrt(torch.tensor(edge_probability))
+    adjacency_matrix = torch.minimum(adjacency_matrix, adjacency_matrix.t())
+    adjacency_matrix = adjacency_matrix.to(torch.float32)
+    
+    # Ensure no self-loops
+    adjacency_matrix = adjacency_matrix * (~torch.eye(matrix_size, dtype=torch.bool, device=device))
+    
+    # Generate random weights for the edges
+    weights = torch.rand(size=(matrix_size, matrix_size), dtype=torch.float32, device=device) * 100
+    
+    # Apply weights to the adjacency matrix
+    weighted_adjacency_matrix = adjacency_matrix * weights
+    weighted_adjacency_matrix = torch.maximum(weighted_adjacency_matrix, weighted_adjacency_matrix.t())
+    
+    # Calculate degree matrix
+    degree_matrix = torch.sum(weighted_adjacency_matrix, dim=1)
+    degree_matrix = torch.diag(degree_matrix)
+    
+    # Calculate Laplacian matrix
+    laplacian_matrix = degree_matrix - weighted_adjacency_matrix
+
+    return laplacian_matrix
 
 
 def get_nearest_indices(matrix, indices, K):
     N = matrix.size(1)
     L = len(indices)
-    device = matrix.device
     
     # Compute the Euclidean distance between each row and the input indices row
     input_rows = matrix[indices]
@@ -23,7 +49,9 @@ def get_nearest_indices(matrix, indices, K):
 
 
 def get_cost(matrix, indices, rest_indices, L, K):
-    return train_single_mmf(matrix, L, K, indices.unsqueeze(-1).tolist(), rest_indices.tolist(), epochs=0, learning_rate=1e-1, early_stop=False)[3]
+    dim = matrix.size(0) - L
+    matrix_rec = learnable_mmf_train(matrix, L = L, K = K, drop = 1, dim = dim, wavelet_indices = indices.unsqueeze(-1).tolist(), rest_indices = rest_indices.tolist(), epochs = 0, learning_rate = 1e-4, early_stop = True)[0]
+    return torch.norm(matrix - matrix_rec, p = 'fro').item()
 
 
 def crossover(parent1, parent2):
@@ -134,6 +162,77 @@ def evolutionary_algorithm(cost_function, matrix, L, K, population_size=1000, ge
     if min_cost < all_time_min_cost:
         all_time_min_cost = min_cost
         all_time_best_solution = best_solution
-    all_time_min_cost_per_gen.append(all_time_min_cost)
+    
+    return all_time_best_solution, nearest_indices[all_time_best_solution, :], all_time_min_cost, min_cost_per_gen, mean_cost_per_gen, all_time_min_cost_per_gen
+
+
+def directed_evolution(cost_function, matrix, L, K, population_size=100, generations=100, sample_kept_rate=0.3):
+    n = matrix.size(0)
+    device = matrix.device
+    
+    # Initialization
+    population = torch.stack([torch.randperm(n)[:L] for _ in range(population_size)], dim=0)
+    nearest_indices = get_nearest_indices(matrix, torch.arange(n), K - 1)
+
+    min_cost_per_gen = []
+    mean_cost_per_gen = []
+    all_time_min_cost_per_gen = []
+    all_time_min_cost = float('inf')
+    all_time_best_solution = None
+    
+    for gen in range(generations):
+        # Evaluation
+        costs = torch.tensor([cost_function(matrix, indices, nearest_indices[indices, :], L, K) for indices in population], device=device)
+        min_cost_idx = torch.argmin(costs)
+        best_solution = population[min_cost_idx]
+
+        print(f'The best solution at generation {gen} is {torch.min(costs, dim=0)[0].item()}')
+        min_cost_per_gen.append(torch.min(costs, dim=0)[0].item())
+        mean_cost_per_gen.append(torch.mean(costs, dim=0).item())
+
+        if min_cost_per_gen[-1] < all_time_min_cost:
+            all_time_min_cost = min_cost_per_gen[-1]
+            all_time_best_solution = best_solution
+
+        all_time_min_cost_per_gen.append(all_time_min_cost)
+        
+        # Selection
+        sorted_indices = torch.argsort(costs)
+        selected_indices = sorted_indices[: math.floor(population_size * sample_kept_rate)]
+        selected_population = population[selected_indices]
+
+        for s in selected_population:
+            assert len(s.tolist()) == len(set(s.tolist()))
+        
+        # Diversification
+        offspring_population = []
+        for i in range(len(selected_population)):
+            for _ in range(population_size // len(selected_population) + 1):
+                mutation_indices = torch.randperm(L)[:2]  # Select 2 random mutation indices
+                mutated_sample = selected_population[i].clone()  # Make a copy of the sample
+                # Swap the values at the mutation indices
+                mutated_sample[mutation_indices[0]], mutated_sample[mutation_indices[1]] = \
+                    mutated_sample[mutation_indices[1]].item(), mutated_sample[mutation_indices[0]].item()
+            
+                mutation_idx = torch.randint(L, (1,))
+                pool = torch.from_numpy(np.setdiff1d(torch.arange(n).numpy(), mutated_sample.numpy()))
+                random_index = torch.randint(len(pool), (1,))
+                random_number = pool[random_index]
+                mutated_sample[mutation_idx] = random_number.item()
+                offspring_population.append(mutated_sample)
+
+        random.shuffle(offspring_population)
+        offspring_population = offspring_population[: population_size - len(selected_population)]
+        population = torch.stack(offspring_population + [sample for sample in selected_population])
+        assert len(population) == population_size
+    
+    # Final evaluation
+    final_costs = torch.tensor([cost_function(matrix, indices, nearest_indices[indices, :], L, K) for indices in population], device=device)
+    min_cost_idx = torch.argmin(final_costs)
+    best_solution = population[min_cost_idx]
+    min_cost = final_costs[min_cost_idx]
+    if min_cost < all_time_min_cost:
+        all_time_min_cost = min_cost
+        all_time_best_solution = best_solution
     
     return all_time_best_solution, nearest_indices[all_time_best_solution, :], all_time_min_cost, min_cost_per_gen, mean_cost_per_gen, all_time_min_cost_per_gen
--- source/test_metaheuristics.py
+++ source/test_metaheuristics.py
@@ -3,8 +3,7 @@ import time
 import random
 import unittest
 import matplotlib.pyplot as plt
-from metaheuristics import get_cost, evolutionary_algorithm
-from utils import generate_single_random_weighted_graph_laplacian
+from metaheuristics import get_cost, evolutionary_algorithm, generate_single_random_weighted_graph_laplacian, directed_evolution
 
 class TestGenerateSingleRandomWeightedGraphLaplacian(unittest.TestCase):
     def test_output_shape(self):
@@ -94,6 +93,61 @@ class TestMetaheuristics(unittest.TestCase):
         plt.xlabel('Generation')
         plt.ylabel('Cost')
         plt.title('Best and mean evaluation values of EA')
+        plt.legend()  # Adding legend
+
+        # Display plot
+        plt.show()
+
+        # Keep the plot window open until the user exits
+        while True:
+            plt.pause(0.05)  # Pause for 0.05 seconds
+
+    def test_directed_evolution(self):
+        # Define dummy input parameters
+        matrix_size = 60
+        edge_probability = 0.6
+        device = 'cpu'
+
+        # Call the function
+        laplacian_matrix = generate_single_random_weighted_graph_laplacian(matrix_size, edge_probability)
+        L = 40
+        K = 8
+
+        wavelet_indices = torch.tensor(random.sample(range(matrix_size), L))
+        rest_indices = torch.zeros(L, K - 1, dtype=torch.int64)
+        for j in range(L):
+            values = list(range(matrix_size))
+            values.remove(wavelet_indices[j].item())
+            rest_indices[j] = torch.tensor(random.sample(values, K - 1))
+
+        cost = get_cost(laplacian_matrix, wavelet_indices, rest_indices, L, K)
+        print(f'The random cost is {cost}')
+        start_time = time.time()
+
+        wavelet_indices, rest_indices, de_cost, min_cost_per_gen, mean_cost_per_gen, all_time_min_cost_per_gen = directed_evolution(get_cost, laplacian_matrix, L, K, population_size=100, generations=100, sample_kept_rate=0.3)
+
+        # Record the end time
+        end_time = time.time()
+
+        # Calculate the runtime
+        runtime = end_time - start_time
+
+        print("Runtime:", runtime, "seconds")
+
+        print(f'The best DE cost is {de_cost}')
+        print(f'The wavelets is {wavelet_indices}, rest_indices {rest_indices}')
+
+        # Sample data
+
+        # Plotting
+        # plt.plot(range(len(min_cost_per_gen)), min_cost_per_gen, label='Min evaluation')
+        plt.plot(range(len(mean_cost_per_gen)), mean_cost_per_gen, label='Mean evaluation')
+        plt.plot(range(len(all_time_min_cost_per_gen)), all_time_min_cost_per_gen, label='Best evaluation')
+
+        # Adding labels and title
+        plt.xlabel('Generation')
+        plt.ylabel('Cost')
+        plt.title('Best and mean evaluation values of DE')
         plt.legend()  # Adding legend
 
         # Display plot
