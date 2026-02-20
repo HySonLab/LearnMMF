fb257acc66c117ebbfff2c80ca47c4460dda360a
nguyenthieukhang <49133713+nguyenthieukhang@users.noreply.github.com>
2026-02-20 21:42:01 +0800

    Update run_all.sh

--- experiments/node_classification/run_all.sh
+++ experiments/node_classification/run_all.sh
@@ -4,6 +4,15 @@ DATASETS=("cora" "citeseer")
 SPLITS=("MMF1" "MMF2" "MMF3")
 
 for dataset in "${DATASETS[@]}"; do
+    python main.py \
+      --dataset "$dataset" \
+      --dim 2 \
+      --seed 42 \
+      --output-dir "./${dataset}_wavelets" \
+      --save-wavelets
+done
+
+for dataset in "${DATASETS[@]}"; do
   for split in "${SPLITS[@]}"; do
     echo "Running dataset=$dataset split=$split"
     python main.py \
@@ -11,7 +20,8 @@ for dataset in "${DATASETS[@]}"; do
       --dim 2 \
       --split "$split" \
       --seed 42 \
-      --output-dir "./${dataset}_${split,,}"
+      --output-dir "./${dataset}_${split,,}" \
+      --load-wavelets "./${dataset}_wavelets/wavelets"
   done
 done
 
